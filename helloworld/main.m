258dbaaa3c595adbcdf418c7e04d66e9624154df
魏志鹏 <weizhipeng@weizhipengdemacbook-pro.local>
2015-12-22 15:47:19 +0800

    Initial Commit

---
+++ helloworld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by 魏志鹏 on 15/12/22.
+//  Copyright © 2015年 wzp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
